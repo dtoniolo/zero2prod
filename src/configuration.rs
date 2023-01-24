980ca227cc32289c6030a8197d54736a90879bff
dtoniolo <d.toniolo2@campus.unimib.it>
2023-01-24 17:09:03 +0100

    Completes the first integration test

--- src/configuration.rs
+++ src/configuration.rs
@@ -33,4 +33,14 @@ impl DatabaseSettings {
             self.database_name
         )
     }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username,
+            self.password,
+            self.host,
+            self.port
+        )
+    }
 }
